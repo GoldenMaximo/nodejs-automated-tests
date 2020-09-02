78368309aef11e725eaed8b476f8a0f9877f160e
Gustavo Máximo <gfmaximo97@gmail.com>
2020-09-02 16:11:16 -0300

    feat: Connect to MongoDB. Create Post model.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const feedRoutes = require('./routes/feed');
+
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygqkk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 const app = express();
 
@@ -16,4 +19,6 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen('8080');
+mongoose.connect(MONGODB_URI).then(result => {
+    app.listen('8080');
+}).catch(err => console.log('ATLAS CONNECTION ERROR: ', err));
