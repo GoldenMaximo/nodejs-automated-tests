717503404afd79a4ee6bb471ed28d8c5f1c418a2
Gustavo Máximo <gfmaximo97@gmail.com>
2020-09-02 15:39:43 -0300

    feat: Add post route validation.

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     // TODO: get db posts here
     res.status(200).json({
@@ -15,6 +17,13 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(402).json({
+            message: 'Validation failed - Entered data is incorrect',
+            errors: errors.array()
+        })
+    }
     const { title, content } = req.body;
     // TODO: create post in db here
     res.status(201).json({
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 
 const feedControler = require('../controllers/feed');
 
 router.get('/posts', feedControler.getPosts);
 
-router.post('/post', feedControler.createPost);
+router.post('/post', [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+], feedControler.createPost);
 
 module.exports = router;
