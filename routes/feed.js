80b0e4408611549aed8ea4385c37c6632c9abfb2
Gustavo Máximo <gfmaximo97@gmail.com>
2020-09-10 10:00:22 -0300

    feat: Add auth to all routes.

--- routes/feed.js
+++ routes/feed.js
@@ -7,18 +7,18 @@ const feedControler = require('../controllers/feed');
 
 router.get('/posts', isAuth, feedControler.getPosts);
 
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),
 ], feedControler.createPost);
 
-router.get('/post/:postId', feedControler.getPost);
+router.get('/post/:postId', isAuth, feedControler.getPost);
 
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth, [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),
 ], feedControler.putEditPost);
 
-router.delete('/post/:postId', feedControler.deletePost);
+router.delete('/post/:postId', isAuth, feedControler.deletePost);
 
 module.exports = router;
