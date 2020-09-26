b945c49602c8672d77504d16ab24ddfd268721f1
Gustavo Máximo <gfmaximo97@gmail.com>
2020-09-25 21:00:03 -0300

    refactor: Sort Posts according to creation date.

--- controllers/feed.js
+++ controllers/feed.js
@@ -12,7 +12,7 @@ exports.getPosts = async (req, res, next) => {
     const perPage = 2;
     try {
         const totalItems = await Post.find().countDocuments();
-        const posts = await Post.find().populate('creator').skip((page - 1) * perPage).limit(perPage);
+        const posts = await Post.find().sort({ createdAt: -1 }).populate('creator').skip((page - 1) * perPage).limit(perPage);
 
         return res.status(200).json({
             message: 'Posts fetched successefully',
