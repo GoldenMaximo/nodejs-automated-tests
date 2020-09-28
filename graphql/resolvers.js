5266409fc3920a3735ac9efbc321776f41480c5b
Gustavo Máximo <gfmaximo97@gmail.com>
2020-09-28 15:38:22 -0300

    feat: Define mutation schema and add mutation resolver for user model. Enable graphiql.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,29 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-    hello() {
+    createUser: async function({ userInput }, req) {
+        // const email = args.userInput.email;
+        const existingUser = await User.findOne({ email: userInput.email });
+
+        if (existingUser) {
+            const error = new Error('User already exists!');
+            throw error;
+        }
+
+        const hashedPassword = await bcrypt.hash(userInput.password, 12);
+
+        const user = new User({
+            email: userInput.email,
+            name: userInput.name,
+            password: hashedPassword
+        });
+
+        const createdUser = await user.save();
+
         return {
-            text: 'Hello world',
-            views: 1
-        };
+            ...createdUser._doc,
+            _id: createdUser._id.toString()
+        }
     }
 };
