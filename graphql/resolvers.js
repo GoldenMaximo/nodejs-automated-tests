c19c7b7c1ff3513f33475b62554208f5a5eec93a
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-03 01:17:59 -0300

    feat: Add pagination on Posts query.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -140,18 +140,27 @@ module.exports = {
         };
     },
 
-    posts: async function (args, req) {
+    posts: async function ({ page }, req) {
         if (!req.isAuth) {
             const error = new Error('User is not authenticated');
             error.code = 401;
             throw error;
         }
 
+        if (!page) {
+            page = 1;
+        }
+
+        const perPage = 2;
+
         const totalPosts = await Post.find().countDocuments();
 
-        const posts = await Post.find().sort({
-            createdAt: -1
-        }).populate('creator');
+        const posts = await Post
+            .find()
+            .sort({ createdAt: -1 })
+            .skip((page - 1) * perPage)
+            .limit(2)
+            .populate('creator');
 
         return {
             posts: posts.map(e => {
