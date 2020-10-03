066c78872938ea8c25a82e0a021b143461c3d49b
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-03 16:23:57 -0300

    feat: Add file upload endpoint. Add helper func to clean old files.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const auth = require('./middlewares/auth');
+const { clearImage } = require('./helpers/helper-funcs');
 
 const { graphqlHTTP } = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
@@ -46,6 +47,19 @@ app.use((req, res, next) => {
         return res.sendStatus(200);
     }
     next();
+});
+
+app.put('/post-image', (req, res, next) => {
+    if (!req.isAuth) {
+        throw new Error('User is not authenticated.');
+    }
+    if (!req.file) {
+        return res.status(200).json({ message: 'No file provided.' });
+    }
+    if (req.body.oldPath) {
+        clearImage(req.body.oldPath);
+    }
+    return res.status(201).json({ message: 'File stored.', filePath: req.file.path });
 });
 
 app.use(auth);
