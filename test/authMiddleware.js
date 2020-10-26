304cb2972ff5d0f2d7442d0beb6506a242d77d13
Gustavo Máximo <gfmaximo97@gmail.com>
2020-10-26 15:12:11 -0300

    feat: Add test for auth middleware. Group tests with description.

--- test/authMiddleware.js
+++ test/authMiddleware.js
@@ -2,12 +2,24 @@ const { expect } = require('chai');
 
 const authMiddleware = require('../middlewares/is-auth');
 
-it('Should throw an error if no authorization header is present', () => {
-    const req = {
-        get: (headerName) => {
-            return null;
-        }
-    };
+describe('Auth middleware', () => {
+    it('Should throw an error if no authorization header is present', () => {
+        const req = {
+            get: (headerName) => {
+                return null;
+            }
+        };
 
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Unauthorized');
-});
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Unauthorized');
+    });
+
+    it('Should throw an error if the authorization header is only one string', () => {
+        const req = {
+            get: (headerName) => {
+                return 'test';
+            }
+        };
+
+        expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
+})
